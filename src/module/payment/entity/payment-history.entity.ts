8c5b0ffddd0fee5b54009054f27417e72614b5e1
Elaboro <kviynt@mail.ru>
2024-02-17 16:25:26 +0300

    Fix payment history entity, add decorator

--- src/module/payment/entity/payment-history.entity.ts
+++ src/module/payment/entity/payment-history.entity.ts
@@ -1,7 +1,8 @@
-import { BaseEntity, Column, CreateDateColumn, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { UserEntity } from "../../user/entity/user.entity";
 import { IPaymentHistory, PaymentAction } from "../interface";
 
+@Entity({ name: "payment_history" })
 export class PaymentHistoryEntity extends BaseEntity implements IPaymentHistory {
   @PrimaryGeneratedColumn("uuid")
   payment_history_id: string;
